3d89861c317eabc70ed1609e181a1adf6f82a516
joao avila <joaoavila.py@gmail.com>
2022-12-30 12:25:14 -0300

    inclusão de alguns campos

--- src/components/pix/Pix.jsx
+++ src/components/pix/Pix.jsx
@@ -1,13 +1,48 @@
 import React from 'react'
 import Grid from '@mui/material/Grid'
-//import TextField from '@mui/material/TextField'
+import '@fontsource/roboto/300.css'
+import Typography from '@mui/material/Typography'
+import TextField from '@mui/material/TextField'
+
 
 export default function Pix() {
     return (
         <>
-        <Grid container justifyContent={'center'}>
-        skjdfldksj
-        </Grid>
+            <Grid container justifyContent={'center'} sx={{ bgcolor: 'black' }}>
+                <Typography mt={3} fontFamily={'roboto'} color={'white'}>
+                    React Pix
+                </Typography>
+            </Grid>
+            <br />
+            <Grid container justifyContent={'center'} spacing={3}>
+
+                <Grid item>
+
+                    <TextField id="nomeFavorecido"
+                        fullwidth
+                        label="Nome do Favorecido"
+                        variant="outlined"
+                    />
+
+                </Grid>
+                <Grid item>
+
+                    <TextField id="cidadeFavorecido"
+                        fullwidth
+                        label="Cidade do Favorecido"
+                        variant="outlined"
+                    />
+
+                </Grid>
+                <Grid item>
+
+                    <TextField id="valoPix"
+                        fullwidth
+                        label="Valor R$"
+                        variant="outlined"
+                    />
+                </Grid>
+            </Grid>
         </>
     )
 }
